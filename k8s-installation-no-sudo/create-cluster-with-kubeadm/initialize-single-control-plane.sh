2d7ed1e2c562301db4bb55ba23c77637213f8bcd
anami-sf <anami.127.0.0.1@gmail.com>
2024-02-25 13:13:18 -0800

    Update initialize controlplane

--- k8s-installation-no-sudo/create-cluster-with-kubeadm/initialize-single-control-plane.sh
+++ k8s-installation-no-sudo/create-cluster-with-kubeadm/initialize-single-control-plane.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 ip route show # Look for a line starting with "default via"
 
 echo "-------------------------------------------------------------------------" 
@@ -7,12 +9,12 @@ echo "Initialize control-plane"
 echo "-------------------------------------------------------------------------" 
 # Considerations about apiserver-advertise-address and ControlPlaneEndpoint
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#considerations-about-apiserver-advertise-address-and-controlplaneendpoint
-kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=10.0.0.213 &> join-pods-token
+kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=10.0.0.213
 
 echo "-------------------------------------------------------------------------" 
 echo 'From k8 control-plane node follow instructions in "join-pods-token"'
 echo "-------------------------------------------------------------------------"
 
-  mkdir -p $HOME/.kube
-  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
