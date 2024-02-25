df4704582d5a1ced5b2bdab98818c047194bfc23
anami-sf <anami.127.0.0.1@gmail.com>
2024-02-24 21:16:43 -0800

    Initialize controlplane

--- k8s-installation-no-sudo/create-cluster-with-kubeadm/initialize-single-control-plane.sh
+++ k8s-installation-no-sudo/create-cluster-with-kubeadm/initialize-single-control-plane.sh
@@ -7,7 +7,7 @@ echo "Initialize control-plane"
 echo "-------------------------------------------------------------------------" 
 # Considerations about apiserver-advertise-address and ControlPlaneEndpoint
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#considerations-about-apiserver-advertise-address-and-controlplaneendpoint
-kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=10.0.0.216 &> join-pods-token
+kubeadm init --pod-network-cidr=192.168.0.0/16 &> join-pods-token
 
 echo "-------------------------------------------------------------------------" 
 echo 'From k8 control-plane node follow instructions in "join-pods-token"'
--- ssh-to-remote/github-ssh.sh
+++ ssh-to-remote/github-ssh.sh
@@ -2,13 +2,10 @@
 
 set -x
 
-# Configfile
-. copy-files-to-remote/node.config
-
 # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 
 # Path to private ssh key
-public_key_file_path=~/.ssh/id_ed25519_github
+public_key_file_path=$HOME/.ssh/id_ed25519_github
 
 # Generate key pair
 ssh-keygen -t ed25519 -C "anami.127.0.0.1@gmail.com" -N "" -f $public_key_file_path 
@@ -19,3 +16,9 @@ eval "$(ssh-agent -s)"
 ssh-add $public_key_file_path
 
 cat "$public_key_file_path.pub"
+
+git config --global user.name "anami-sf"
+
+git config --global user.email "anami.127.0.0.1@gmail.com"
+
+vi ~/.ssh/config
