309e1a1fdab529309462558bc3b405c166c9a85d
anami-sf <anami.127.0.0.1@gmail.com>
2024-03-01 21:19:23 -0800

    Remove unnecessary update

--- k8s-installation-no-sudo/k8-install-crio-all-nodes.sh
+++ k8s-installation-no-sudo/k8-install-crio-all-nodes.sh
@@ -102,5 +102,4 @@ mkdir -p /usr/share/keyrings
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | gpg --dearmor -o /usr/share/keyrings/libcontainers-archive-keyring.gpg
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | gpg --dearmor -o /usr/share/keyrings/libcontainers-crio-archive-keyring.gpg
 
-apt-get update
 apt-get install cri-o cri-o-runc
