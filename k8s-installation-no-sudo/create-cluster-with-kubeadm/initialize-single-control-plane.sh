8d9ddd6bdf4db80f2be44e42901cd5da6c47cc4a
anami-sf <anami.127.0.0.1@gmail.com>
2024-03-03 16:30:40 -0800

    Update --pod-network-cider for flannel

--- k8s-installation-no-sudo/create-cluster-with-kubeadm/initialize-single-control-plane.sh
+++ k8s-installation-no-sudo/create-cluster-with-kubeadm/initialize-single-control-plane.sh
@@ -9,7 +9,7 @@ echo "Initialize control-plane"
 echo "-------------------------------------------------------------------------" 
 # Considerations about apiserver-advertise-address and ControlPlaneEndpoint
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#considerations-about-apiserver-advertise-address-and-controlplaneendpoint
-kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=10.0.0.213
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.0.0.213
 
 echo "-------------------------------------------------------------------------" 
 echo 'From k8 control-plane node follow instructions in "join-pods-token"'
