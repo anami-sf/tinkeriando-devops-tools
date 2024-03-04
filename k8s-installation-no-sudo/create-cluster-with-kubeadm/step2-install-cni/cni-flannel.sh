ab8ea23bec55b4c62e5dd7fe6d4bf8814fe95312
anami-sf <anami.127.0.0.1@gmail.com>
2024-03-03 18:03:41 -0800

    flannel script

---
+++ k8s-installation-no-sudo/create-cluster-with-kubeadm/step2-install-cni/cni-flannel.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# https://github.com/flannel-io/flannel?tab=readme-ov-file#deploying-flannel-with-kubectl
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
