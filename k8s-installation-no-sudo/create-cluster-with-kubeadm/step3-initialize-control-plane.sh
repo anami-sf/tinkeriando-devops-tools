9c819b7ad826f9888381ddfe636bba360d3c42ac
anami-sf <anami.127.0.0.1@gmail.com>
2024-03-03 17:02:24 -0800

    Add netowork cidr options for calico and flannel

--- k8s-installation-no-sudo/create-cluster-with-kubeadm/step3-initialize-control-plane.sh
+++ k8s-installation-no-sudo/create-cluster-with-kubeadm/step3-initialize-control-plane.sh
@@ -9,7 +9,14 @@ echo "Initialize control-plane"
 echo "-------------------------------------------------------------------------" 
 # Considerations about apiserver-advertise-address and ControlPlaneEndpoint
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#considerations-about-apiserver-advertise-address-and-controlplaneendpoint
+
+# IMPORTANT: Choose the appropriate network CIDR below 
+
+# For FLANNEL !!
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.0.0.213
+
+# For CALICO !!
+# kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=10.0.0.213
 
 echo "-------------------------------------------------------------------------" 
 echo 'From k8 control-plane node follow instructions in "join-pods-token"'
